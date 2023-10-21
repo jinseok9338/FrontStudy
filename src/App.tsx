a59ac4a457a514d381200c72079cd2213a7f1787
ccthecode <ccthecode@gmail.com>
2023-10-21 13:46:04 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+function App() {
+
+  return (
+    <div className="my-6 max-w-6xl mx-auto">
+      <h1 className="font-bold tracking-tighter text-3xl">React App Starter template</h1>
+      <p className="my-3 font-normal text-lg"> This template includes:</p>
+      <ol>
+        <li className="list-decimal ml-6">vite</li>
+        <li className="list-decimal ml-6">react-router-dom</li>
+        <li className="list-decimal ml-6">tailwindcss</li>
+        <li className="list-decimal ml-6">postcss </li>
+        <li className="list-decimal ml-6">autoprefixer</li>
+        <li className="list-decimal ml-6">typescript</li>
+      </ol>
+    </div>
+  )
+}
+
+export default App
