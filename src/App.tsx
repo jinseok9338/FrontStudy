ac580aba73e21028e3ec05bfd666d6546d8ce5af
anchaewon <cwahn@anchors-biz.com>
2024-10-28 15:16:05 +0900

    ui 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
+import TodoList from "./component/TodoList";
 
 function App() {
-
   return (
-    <div className="my-6 max-w-6xl mx-auto">
-      <h1 className="font-bold tracking-tighter text-3xl">React App Starter template</h1>
-      <p className="my-3 font-normal text-lg"> This template includes:</p>
-      <ol>
-        <li className="list-decimal ml-6">vite</li>
-        <li className="list-decimal ml-6">react-router-dom</li>
-        <li className="list-decimal ml-6">tailwindcss</li>
-        <li className="list-decimal ml-6">postcss </li>
-        <li className="list-decimal ml-6">autoprefixer</li>
-        <li className="list-decimal ml-6">typescript</li>
-      </ol>
-    </div>
-  )
+    <>
+      <TodoList />
+    </>
+  );
 }
 
-export default App
+export default App;
